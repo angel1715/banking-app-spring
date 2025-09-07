782eb34763df87ac36bfbbe2484ee597844f6504
Angel <angel17@gmail.com>
2025-09-07 11:46:59 -0400

    updated the cross-origin

--- src/main/java/com/ag/banking/app/Banking/App/restController/UserRestController.java
+++ src/main/java/com/ag/banking/app/Banking/App/restController/UserRestController.java
@@ -23,7 +23,7 @@ import java.util.*;
 @RestController
 @RequestMapping("/banking")
 @EnableMethodSecurity
-@CrossOrigin(value = {"https://banking-app-front.vercel.app/", "http://localhost:5173/"})
+@CrossOrigin(value = {"https://banking-app-front.vercel.app/", "http://localhost:5174/"})
 public class UserRestController {
 
     @Autowired
